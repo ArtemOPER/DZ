0c3d4c33df08b5fa52782cde5d50a8f37c08c5f1
Glan Artem <artemglan@mail.ru>
2021-02-13 00:55:49 +0400

    Добавьте файлы проекта.

---
+++ ДЗ 15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ДЗ_15
+{
+    class Program
+    {
+
+        static void PrintArray(int[] myArray, int i = 0)
+        {
+            if (i< myArray.Length)
+            {
+                Console.WriteLine(myArray[i]);
+                PrintArray(myArray, i + 1);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] myArray = { 5, 3, 2 };
+
+            PrintArray(myArray);
+        }
+    }
+}
